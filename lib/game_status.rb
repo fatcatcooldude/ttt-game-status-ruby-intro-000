a318c24b0a8b178cb4242daeccd83a35fe2e4d56
Corey Norton <fatcatcooldude@aol.com>
2020-05-17 19:34:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,13 +1,13 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
-  [6,7,8], # Bottom row
-  [0,3,6], # Left column
-  [1,4,7], # Middle column
-  [2,5,8], # Right column
-  [0,4,8], # Left diagonal
-  [2,4,6]  # Right diagonal
+top_row_win = [0,1,2], # Top row
+middle_row_win = [3,4,5], # Middle row
+bottom_row_win = [6,7,8], # Bottom row
+left_column_win = [0,3,6], # Left column
+middle_column_win = [1,4,7], # Middle column
+right_column_win = [2,5,8], # Right column
+left_diagonal_win = [0,4,8], # Left diagonal
+right_diagonal_win = [2,4,6]  # Right diagonal
 ]
 
 def position_taken?(board, index)
